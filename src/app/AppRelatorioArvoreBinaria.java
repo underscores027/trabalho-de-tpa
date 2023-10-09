4575290b4a0d481b1852e9b41f4a61a533c328f0
Victor Oliveira Santos <victoroliveira34@hotmail.com>
2023-10-09 17:54:47 -0300

    AppRelatorioArvoreBinaria.java dentro dos padrões

--- src/app/AppRelatorioArvoreBinaria.java
+++ src/app/AppRelatorioArvoreBinaria.java
@@ -1,82 +1,64 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+//
+// Source code recreated from a .class file by IntelliJ IDEA
+// (powered by FernFlower decompiler)
+//
+
 package app;
-//Ajuste esse importe para que importe sua classe de Árvore binária
+
+import java.io.PrintStream;
 import lib.ArvoreBinariaExemplo;
 import lib.IArvoreBinaria;
 
-/**
- *
- * @author victoriocarvalho
- * 
- * Classe principal do aplicativo a ser utilizado para fazer o relatório do trabalho 
- * de árvore binária
- */
 public class AppRelatorioArvoreBinaria {
+    public AppRelatorioArvoreBinaria() {
+    }
+
     public static void main(String[] args) {
-        //Instancio o meu gerador de árvores (que também foi fornecido)
         GeradorDeArvores gerador = new GeradorDeArvores();
-        //Instancio um comparador de alunos por matricula (também fornecido)
         ComparadorAlunoPorMatricula comparador = new ComparadorAlunoPorMatricula();
-        IArvoreBinaria<Aluno> arv;
-        
-
-//------Início do trecho citado nas questões 1, 2 e 3 do relatório-------------------------------
-        //Instancio uma árvore binária. Lembre de ajustar o import para sua classe de árvore binária
-        arv = new ArvoreBinariaExemplo(comparador);
-
-        //Chamo o gerador para inserir 100 elementos nessa árvore de forma que fique degenerada
-        gerador.geraArvoreDegenerada(100, arv); //essa parada usa o método adicionar (comentário victor)
+        IArvoreBinaria<Aluno> arv = new ArvoreBinariaExemplo(comparador);
+        gerador.geraArvoreDegenerada(100, arv);
         System.out.println("Árvore Degenerada Criada");
-
-        String resultado = arv.caminharEmNivel();
-        System.out.println("caminhar em nivel:");
-        System.out.println(resultado);
-        System.out.println("caminhar em ordem:");
-        resultado = arv.caminharEmOrdem();
-        System.out.println(resultado);
-        /*
-        //Imprimo a quantidade de nós e a altura da árvore resultante
-        System.out.println("Quantidade de Nós: " + arv.quantidadeNos()+ " Altura: " + arv.altura());
+        PrintStream var10000 = System.out;
+        int var10001 = arv.quantidadeNos();
+        var10000.println("Quantidade de Nós: " + var10001 + " Altura: " + arv.altura());
         arv = new ArvoreBinariaExemplo(comparador);
         gerador.geraArvoreDegenerada(200, arv);
         System.out.println("Árvore Degenerada Criada");
-        System.out.println("Quantidade de Nós: " + arv.quantidadeNos()+ " Altura: " + arv.altura());
+        var10000 = System.out;
+        var10001 = arv.quantidadeNos();
+        var10000.println("Quantidade de Nós: " + var10001 + " Altura: " + arv.altura());
         arv = new ArvoreBinariaExemplo(comparador);
         gerador.geraArvoreDegenerada(1000, arv);
         System.out.println("Árvore Degenerada Criada");
-        System.out.println("Quantidade de Nós: " + arv.quantidadeNos()+ " Altura: " + arv.altura());
-//------Fim do trecho citado nas questões 1, 2 e 3 do relatório-------------------------------
-
-//------Início do trecho citado nas questões 5, 6 e 7 do relatório-------------------------------
+        var10000 = System.out;
+        var10001 = arv.quantidadeNos();
+        var10000.println("Quantidade de Nós: " + var10001 + " Altura: " + arv.altura());
         arv = new ArvoreBinariaExemplo(comparador);
-        //Chamo o gerador para inserir 100 elementos nessa árvore de forma que ela fique perfeitamente balanceada
-        gerador.geraArvorePerfeitamenteBalanceada(1,100,arv);
+        gerador.geraArvorePerfeitamenteBalanceada(1, 100, arv);
         System.out.println("Árvore Perfeitamente Balanceada Criada");
-        System.out.println("Quantidade de Nós: " + arv.quantidadeNos()+ " Altura: " + arv.altura());
+        var10000 = System.out;
+        var10001 = arv.quantidadeNos();
+        var10000.println("Quantidade de Nós: " + var10001 + " Altura: " + arv.altura());
         arv = new ArvoreBinariaExemplo(comparador);
-        gerador.geraArvorePerfeitamenteBalanceada(1,200,arv);
+        gerador.geraArvorePerfeitamenteBalanceada(1, 200, arv);
         System.out.println("Árvore Perfeitamente Balanceada Criada");
-        System.out.println("Quantidade de Nós: " + arv.quantidadeNos()+ " Altura: " + arv.altura());
+        var10000 = System.out;
+        var10001 = arv.quantidadeNos();
+        var10000.println("Quantidade de Nós: " + var10001 + " Altura: " + arv.altura());
         arv = new ArvoreBinariaExemplo(comparador);
-        gerador.geraArvorePerfeitamenteBalanceada(1,1000,arv);
+        gerador.geraArvorePerfeitamenteBalanceada(1, 1000, arv);
         System.out.println("Árvore Perfeitamente Balanceada Criada");
-        System.out.println("Quantidade de Nós: " + arv.quantidadeNos()+ " Altura: " + arv.altura());
-//------Fim do trecho citado nas questões 5, 6 e 7 do relatório-------------------------------
-
-//------Início do trecho citado na questão 9 do relatório-------------------------------
+        var10000 = System.out;
+        var10001 = arv.quantidadeNos();
+        var10000.println("Quantidade de Nós: " + var10001 + " Altura: " + arv.altura());
         arv = new ArvoreBinariaExemplo(comparador);
-        gerador.geraArvorePerfeitamenteBalanceada(1,50000,arv);
+        gerador.geraArvorePerfeitamenteBalanceada(1, 50000, arv);
         System.out.println("Árvore Perfeitamente Balanceada Criada");
         System.out.println("Altura: " + arv.altura());
         arv = new ArvoreBinariaExemplo(comparador);
         gerador.geraArvoreDegenerada(50000, arv);
         System.out.println("Árvore Degenerada Criada");
         System.out.println(" Altura: " + arv.altura());
-//------Fim do trecho citado na questão 9 do relatório-------------------------------
-*/
     }
 }
